98078a90a44d13762b9877d800b1fd3df5653713
Ugochukwu Benedict Nebolisa <nebolisaugochukwu@gmail.com>
2025-03-21 15:38:45 +0100

    feat: how it works component

--- harmony-homes/src/app/page.tsx
+++ harmony-homes/src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import HowItWorks from "@/components/HowItWorks";
 
 export default function Home() {
   return (
     <div>
-      Home
+      <HowItWorks />
     </div>
   );
 }
