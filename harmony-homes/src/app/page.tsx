2b820fd0f6f80469f19c66d6c2ac1905ec22e728
Daniel Nwachukwu <dannyclassic56@gmail.com>
2025-03-22 03:05:50 +0100

    Add "Key Features" Section

--- harmony-homes/src/app/page.tsx
+++ harmony-homes/src/app/page.tsx
@@ -1,9 +1,11 @@
+import KeyFeatures from "@/components/key-features";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       Home
+      <KeyFeatures />
     </div>
   );
 }
