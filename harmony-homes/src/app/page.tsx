5eb03ea5be5a08d9093365009c0b6954ddce1840
Angel Mba <70325824+Michelle031@users.noreply.github.com>
2025-03-24 23:11:37 +0100

    Fix 7 (#15)
    * added nav links
    
    * fixed issue #7

--- harmony-homes/src/app/page.tsx
+++ harmony-homes/src/app/page.tsx
@@ -1,5 +1,6 @@
 import HowItWorks from "@/components/HowItWorks";
 import KeyFeatures from "@/components/key-features";
+import Header from "@/components/Navbar/Navbar";
 
 //import Image from "next/image";
 import Navbar from "@/components/Navbar";
@@ -7,6 +8,7 @@ import KeyFeatures from "@/components/key-features";
 export default function Home() {
   return (
     <div>
+      <Header />
       <Navbar />
       <HowItWorks />
       <KeyFeatures />
