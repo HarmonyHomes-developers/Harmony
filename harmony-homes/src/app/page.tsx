65a821f78d2dee07ceebb4eee93ce5c0f3f11d0f
Elite-tch <149904417+Elite-tch@users.noreply.github.com>
2025-03-24 22:43:54 +0100

    created Log in and Sign up button (#10)
    * created Log in and Sign up button
    
    * error

--- harmony-homes/src/app/page.tsx
+++ harmony-homes/src/app/page.tsx
@@ -1,9 +1,13 @@
 import HowItWorks from "@/components/HowItWorks";
 import KeyFeatures from "@/components/key-features";
 
+//import Image from "next/image";
+import Navbar from "@/components/Navbar";
+import KeyFeatures from "@/components/key-features";
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <HowItWorks />
       <KeyFeatures />
     </div>
