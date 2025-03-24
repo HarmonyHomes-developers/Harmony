bbb88588b029dc47cad7981cbf9f68e7b1fb2bb5
Kom02 <kom0211@gmail.com>
2025-03-24 16:16:43 -0600

    correct conflict error

--- harmony-homes/src/app/page.tsx
+++ harmony-homes/src/app/page.tsx
@@ -1,15 +1,10 @@
 import HowItWorks from "@/components/HowItWorks";
-import KeyFeatures from "@/components/key-features";
 import Header from "@/components/Navbar/Navbar";
-
-//import Image from "next/image";
-import Navbar from "@/components/Navbar";
 import KeyFeatures from "@/components/key-features";
 export default function Home() {
   return (
     <div>
       <Header />
-      <Navbar />
       <HowItWorks />
       <KeyFeatures />
     </div>
