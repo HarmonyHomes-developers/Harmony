e27be74d90e98086f792adf4d275371b494e1da2
Kom02 <kom0211@gmail.com>
2025-03-25 17:57:40 -0600

    correct conflict error (#17)

--- harmony-homes/src/app/page.tsx
+++ harmony-homes/src/app/page.tsx
@@ -3,7 +3,6 @@ import Header from "@/components/Navbar/Navbar";
 import KeyFeatures from "@/components/key-features";
 import RealEstateCTA from "@/components/RealEstateCTA";
 import Image from "next/image";
-import Header from "@/components/Navbar/Navbar";
 
 export default function Home() {
   return (
