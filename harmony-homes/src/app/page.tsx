350d4bc3139bd40660c162190e213a9ad2b8aacc
Angel Mba <70325824+Michelle031@users.noreply.github.com>
2025-03-25 15:40:34 +0100

    added nav links (#13)
    Co-authored-by: Kom02 <kom0211@gmail.com>

--- harmony-homes/src/app/page.tsx
+++ harmony-homes/src/app/page.tsx
@@ -1,6 +1,8 @@
 import HowItWorks from "@/components/HowItWorks";
 import Header from "@/components/Navbar/Navbar";
 import KeyFeatures from "@/components/key-features";
+import Header from "@/components/Navbar/Navbar";
+
 export default function Home() {
   return (
     <div>
