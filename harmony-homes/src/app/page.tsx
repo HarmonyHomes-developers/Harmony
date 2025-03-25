28f403ea7b4229867b944d515ee5df3d7ea326d1
Collins Ikechukwu <collinschristroa@gmail.com>
2025-03-25 15:43:28 +0100

    Feature/create real estate cta (#12)
    * feat: add RealEstateCTA component and binance image to homepage
    
    * refactor: replace binance image with placeholder in RealEstateCTA component

--- harmony-homes/src/app/page.tsx
+++ harmony-homes/src/app/page.tsx
@@ -1,6 +1,8 @@
 import HowItWorks from "@/components/HowItWorks";
 import Header from "@/components/Navbar/Navbar";
 import KeyFeatures from "@/components/key-features";
+import RealEstateCTA from "@/components/RealEstateCTA";
+import Image from "next/image";
 import Header from "@/components/Navbar/Navbar";
 
 export default function Home() {
@@ -9,6 +11,7 @@ export default function Home() {
       <Header />
       <HowItWorks />
       <KeyFeatures />
+      <RealEstateCTA />
     </div>
   );
 }
