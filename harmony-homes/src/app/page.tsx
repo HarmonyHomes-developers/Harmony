e6f6a3ed3727738016b0d5ec321fa78d8ff3ad6c
Okeke Chinedu Emmanuel <chiscookeke11@gmail.com>
2025-03-26 01:08:40 +0100

    Add Transparent Real Estate Transactions Module  (#14)
    * hero section
    
    * fixes
    
    * fix
    
    * fix
    
    ---------
    
    Co-authored-by: Kom02 <kom0211@gmail.com>

--- harmony-homes/src/app/page.tsx
+++ harmony-homes/src/app/page.tsx
@@ -1,16 +1,22 @@
+
 import HowItWorks from "@/components/HowItWorks";
 import Header from "@/components/Navbar/Navbar";
+
 import KeyFeatures from "@/components/key-features";
 import RealEstateCTA from "@/components/RealEstateCTA";
+import Hero from "./components/Hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div >
       <Header />
+      <Hero />
       <HowItWorks />
+
       <KeyFeatures />
       <RealEstateCTA />
     </div>
+
   );
 }
