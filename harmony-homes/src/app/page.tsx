8be9615d3bd8e3731189e4d3b1b15f27e0a8b246
Kom02 <kom0211@gmail.com>
2025-04-09 16:59:51 -0600

    refactor and update of libraries

--- harmony-homes/src/app/page.tsx
+++ harmony-homes/src/app/page.tsx
@@ -3,7 +3,7 @@ import Header from "@/components/organisms/navbar";
 import KeyFeatures from "@/components/organisms/key-features";
 import Image from "next/image";
 import RealEstateCTA from "@/components/molecules/realEstateCTA";
-import Hero from "./components/hero";
+import Hero from "../components/molecules/hero";
 import HowItWorks from "@/components/organisms/howItWorks";
 
 export default function Home() {
