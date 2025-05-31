b01f66bf213d5aad8e01dbcf54f85cb7fa74c7d9
JamesVictor-o <victorjames40v@gmail.com>
2025-05-31 12:34:49 +0100

    implement the seller details page

--- harmony-homes/src/app/seller/page.tsx
+++ harmony-homes/src/app/seller/page.tsx
@@ -7,7 +7,7 @@ const SellerDetailPage: React.FC = () => {
   return (
     <div className="min-h-screen bg-gray-50">
     <Header/>
-      <div className=" mx-auto p-6">
+      <div className=" mx-auto p-6 md:px-28">
         <button className="flex items-center text-gray-600 mb-6 hover:text-gray-800 transition-colors">
           <span className="mr-2">←</span>
           Back to Properties
